6394602f77928cfe6c936dc7cf5dd594909d82b3
Pablo Fernandes <pablofern87@gmail.com>
2020-02-02 10:52:25 -0300

    feat: - connectando ao mongo db

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import databaseConfig from '../config/database';
 import User from '../app/models/user.model';
 import File from '../app/models/file.model';
@@ -9,6 +10,7 @@ const models = [User, File, Appointment];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -19,6 +21,17 @@ class Database {
                 model =>
                     model.associate && model.associate(this.connection.models)
             );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/gobarber',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: true,
+                useUnifiedTopology: true,
+            }
+        );
     }
 }
 
