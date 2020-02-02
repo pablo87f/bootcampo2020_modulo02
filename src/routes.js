9b3e55394bd1b5f6d7144f3f497d6a7d8874329b
Pablo Fernandes <pablofern87@gmail.com>
2020-02-01 22:44:10 -0300

    feat: - rota de criação de um agendamento

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/session.controller';
 import authMiddleware from './app/middlewares/auth.middleware';
 import FileController from './app/controllers/file.controller';
 import ProviderController from './app/controllers/provider.controller';
+import AppointmentController from './app/controllers/appointment.controller';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,6 +19,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
