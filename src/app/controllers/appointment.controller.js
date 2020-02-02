3e73e334f5ff6d34d510e25229dfcd2af6531f17
Pablo Fernandes <pablofern87@gmail.com>
2020-02-02 11:15:32 -0300

    feat(notificacao): - Criando schema de notification pro mongodb - Salvando notification no mangodb quando é criado um agendamento

--- src/app/controllers/appointment.controller.js
+++ src/app/controllers/appointment.controller.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/appointment.model';
 import User from '../models/user.model';
 import File from '../models/file.model';
+import Notification from '../schemas/notfication.schema';
 
 class AppointmentController {
     async index(req, res) {
@@ -84,6 +86,17 @@ class AppointmentController {
             user_id: req.userId,
             provider_id,
             date: hourStart,
+        });
+
+        const user = await User.findByPk(req.userId);
+        const formattedDate = format(hourStart, "dd 'de' MMM', às' H:mm'h'", {
+            locale: pt,
+        });
+
+        // Notificar agendamento para o prestador de serviço
+        await Notification.create({
+            content: `Novo agendamento de ${user.name} para o dia ${formattedDate}`,
+            user: provider_id,
         });
 
         return res.json(appointment);
