421c210663e430cf2c27a583b21e4df90c478080
Pablo Fernandes <pablofern87@gmail.com>
2020-02-01 22:56:52 -0300

    feat(agendamento): - verificando disponibilidade e criando validações

--- src/app/controllers/appointment.controller.js
+++ src/app/controllers/appointment.controller.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/appointment.model';
 import User from '../models/user.model';
 
@@ -26,10 +27,34 @@ class AppointmentController {
             });
         }
 
+        // verifica datas passadas
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res.status(401).json({
+                error: 'Past date are not permitted',
+            });
+        }
+
+        // verifica disponibilidade da data
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
+        }
+
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            date,
+            date: hourStart,
         });
 
         return res.json(appointment);
