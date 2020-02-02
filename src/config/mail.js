625374b3113c90d20d5525734a3303c4630368e6
Pablo Fernandes <pablofern87@gmail.com>
2020-02-02 12:32:29 -0300

    feat(email): - configurando envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,17 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'f8278113880be7',
+        pass: '0215d7729b1f2d',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
+// Amazon SES
+// Mailgun
+// Sparkpost
+
+// Mailtrap (DEV)
